e311f2a8c49706050f2b2dd924765b765ff84a29
bobo76 <borisforgood@hotmail.com>
2025-08-11 08:27:27 -0400

    WIP

--- webpage.h
+++ webpage.h
@@ -8,12 +8,12 @@ const char MAIN_page[] PROGMEM = R"rawliteral(
   <title>Temperature API Info</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <style>
+    .card { background: #fff; padding: 2em; border-radius: 1em; width: 90vw; max-width: 500px; box-shadow: 0 4px 16px rgba(0,0,0,0.15); text-align: center; }
+    p, code, pre { font-size: 0.9em; word-break: break-word; }
     body { font-family: Arial, sans-serif; background: #f0f0f0; color: #333; margin: 0; min-height: 100vh; display: flex; justify-content: center; align-items: center; }
-    .card { background: #fff; padding: 2em; border-radius: 16px; width: 90vw; max-width: 500px; box-shadow: 0 4px 16px rgba(0,0,0,0.15); text-align: center; }
     h1 { font-size: 2.2em; }
-    p, code, pre { font-size: 1.2em; word-break: break-word; }
-    pre { background: #f7f9fa; padding: 1.3em; border-radius: 10px; color: #005500; text-align: left; overflow-x: auto; }
-    code { background: #eef4f8; padding: 0.5em 0.5em; border-radius: 5px; font-family: Consolas, monospace; }
+    code { background: #eef4f8; padding: 0.5em 0.5em; border-radius: 0.5em; font-family: Consolas, monospace; }
+    pre { background: #f7f9fa; padding: 1.3em; border-radius: 1em; color: #005500; text-align: left; overflow-x: auto; }
   </style>
 </head>
 <body>
